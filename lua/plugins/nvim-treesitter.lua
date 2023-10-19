1c6311a3ef0e00b69394a7c7bcb4ee58439aca0c
mingleeShade <361977226@qq.com>
2023-10-19 14:33:38 +0800

    禁用 treesitter 的缩进

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,6 +5,8 @@ return {
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "c", "cpp", "ninja", "python", "rst", "toml"})
       end
+      -- 禁用 treesitter 的缩进，其在 cpp 中的表现经常很糟糕
+      opts.indent.enable = false
     end,
   }
 }
