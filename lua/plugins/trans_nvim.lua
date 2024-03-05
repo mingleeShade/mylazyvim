fd4feaf9cde1b4293a6b791520e868a07fd13bec
mingleeShade <wmcibilhm0713h@163.com>
2024-03-05 17:17:35 +0800

    完善注释

--- lua/plugins/trans_nvim.lua
+++ lua/plugins/trans_nvim.lua
@@ -1,6 +1,6 @@
 return {
   {
-    -- 可以主动使用 lua require('Trans').install() 来主动下载
+    -- 可以主动使用 lua require('Trans').install() 来主动下载，第一次下载需要较长时间，如果下载失败，可以删除 ~/.local/share/nvim/lazy/Trans.nvim 目录下的 ultimate.db ultimate.zip 之后，重新下载
     -- 安装 fstival：`sudo apt-get install festival festvox-kallpc16k`
     -- 安装 sqlite3：`sudo apt-get install sqlite3 libsqlite3-dev`
     "JuanZoran/Trans.nvim",
